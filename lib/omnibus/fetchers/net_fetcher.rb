f5b5a11ae2f7632008064646f8aa4b66670557e9
Mathieu Sauve-Frankel <msf@qualcomm.com>
2013-04-30 05:58:20 +0900

    add extractor logic for tar files

--- lib/omnibus/fetchers/net_fetcher.rb
+++ lib/omnibus/fetchers/net_fetcher.rb
@@ -173,6 +173,8 @@ E
     def extract_cmd
       if project_file.end_with?(".gz") || project_file.end_with?(".tgz")
         "gzip -dc  #{project_file} | ( cd #{source_dir} && tar -xf - )"
+      elsif project_file.end_with?(".tar")
+        "tar -zf #{project_file}"
       elsif project_file.end_with?(".bz2")
         "bzip2 -dc  #{project_file} | ( cd #{source_dir} && tar -xf - )"
       elsif project_file.end_with?(".7z")
